453045549da0540a5363073ff2021c0d7f9bd523
Fernando <fernando_redwood@icloud.com>
2023-01-28 11:50:27 -0300

    Update livraria_api_client.py

--- pytests/clients/livraria_api_client.py
+++ pytests/clients/livraria_api_client.py
@@ -8,54 +8,54 @@ class LivrariaClient:
     def post_livros(payload):
         with sync_playwright() as p:
             context = p.request.new_context()
-            response = context.post("http://ec2-34-207-165-114.compute-1.amazonaws.com:3000/livros", data=payload)
+            response = context.post("http://ec2-44-201-222-218.compute-1.amazonaws.com:3000/livros", data=payload)
             LOG.log_info("POST")
-            LOG.log_info("URL: http://ec2-34-207-165-114.compute-1.amazonaws.com:3000/livros")
+            LOG.log_info("URL: http://ec2-44-201-222-218.compute-1.amazonaws.com:3000/livros")
             return {"code": response.status, "body": response.text(), "headers": response.headers}
 
     @staticmethod
     def get_livros():
         with sync_playwright() as p:
             context = p.request.new_context()
-            response = context.get("http://ec2-34-207-165-114.compute-1.amazonaws.com:3000/livros")
+            response = context.get("http://ec2-44-201-222-218.compute-1.amazonaws.com:3000/livros")
             LOG.log_info("GET")
-            LOG.log_info("URL: http://ec2-34-207-165-114.compute-1.amazonaws.com:3000/livros")
+            LOG.log_info("URL: http://ec2-44-201-222-218.compute-1.amazonaws.com:3000/livros")
             return {"code": response.status, "body": response.text(), "headers": response.headers}
 
     @staticmethod
     def get_livros_id():
         with sync_playwright() as p:
             context = p.request.new_context()
-            response = context.get("http://ec2-34-207-165-114.compute-1.amazonaws.com:3000/livros/1")
+            response = context.get("http://ec2-44-201-222-218.compute-1.amazonaws.com:3000/livros/1")
             LOG.log_info("GET")
-            LOG.log_info("URL: http://ec2-34-207-165-114.compute-1.amazonaws.com:3000/livros/1")
+            LOG.log_info("URL: http://ec2-44-201-222-218.compute-1.amazonaws.com:3000/livros/1")
             return {"code": response.status, "body": response.text(), "headers": response.headers}
 
     @staticmethod
     def patch_livros_id(id, payload):
         with sync_playwright() as p:
             context = p.request.new_context()
-            response = context.patch(f"http://ec2-34-207-165-114.compute-1.amazonaws.com:3000/livros/{id}", data=payload)
+            response = context.patch(f"http://ec2-44-201-222-218.compute-1.amazonaws.com:3000/livros/{id}", data=payload)
             LOG.log_info("PATCH")
-            LOG.log_info(f"URL: http://ec2-34-207-165-114.compute-1.amazonaws.com:3000/livros/{id}")
+            LOG.log_info(f"URL: http://ec2-44-201-222-218.compute-1.amazonaws.com:3000/livros/{id}")
             return {"code": response.status, "body": response.text(), "headers": response.headers}
 
     @staticmethod
     def put_livros_id(id, payload):
         with sync_playwright() as p:
             context = p.request.new_context()
-            response = context.put(f"http://ec2-34-207-165-114.compute-1.amazonaws.com:3000/livros/{id}", data=payload)
+            response = context.put(f"http://ec2-44-201-222-218.compute-1.amazonaws.com:3000/livros/{id}", data=payload)
             LOG.log_info("PUT")
-            LOG.log_info(f"URL: http://ec2-34-207-165-114.compute-1.amazonaws.com:3000/livros/{id}")
+            LOG.log_info(f"URL: http://ec2-44-201-222-218.compute-1.amazonaws.com:3000/livros/{id}")
             return {"code": response.status, "body": response.text(), "headers": response.headers}
 
     @staticmethod
     def delete_livros_id(id):
         with sync_playwright() as p:
             context = p.request.new_context()
-            response = context.delete(f"http://ec2-34-207-165-114.compute-1.amazonaws.com:3000/livros/{id}")
+            response = context.delete(f"http://ec2-44-201-222-218.compute-1.amazonaws.com:3000/livros/{id}")
             LOG.log_info("DELETE")
-            LOG.log_info(f"URL: http://ec2-34-207-165-114.compute-1.amazonaws.com:3000/livros/{id}")
+            LOG.log_info(f"URL: http://ec2-44-201-222-218.compute-1.amazonaws.com:3000/livros/{id}")
             return {"code": response.status, "body": response.text(), "headers": response.headers}
 
     @staticmethod
